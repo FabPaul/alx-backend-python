75c80d393b2f148dcd62319e5ef15efdcc74c211
FabPaul <paulfopa0@gmail.com>
2024-01-11 07:55:41 +0100

    Floor anotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" type-anotation function which takes a float as an argument """
+
+
+def floor(n: float) -> int:
+    """ Returns the floor 'int' of the floating point """
+
+    return int(n)
