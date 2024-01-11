8542713d495b213c6fd722dce60b1b75f426dd5d
FabPaul <paulfopa0@gmail.com>
2024-01-11 07:38:32 +0100

    Add anotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" type-anotation function that adds 2 floating point values """
+
+
+def add(a: float, b: float) -> float:
+    """ Returns the sum of a and b as floating point values """
+
+    return a + b
