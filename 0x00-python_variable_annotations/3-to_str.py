cbddb4c31c1aec529ea29a5b506ac5bd8d001348
FabPaul <paulfopa0@gmail.com>
2024-01-11 08:01:08 +0100

    Float to string anotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" Type-anotation function that takes a float n as arg """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string of the floating point """
+
+    return str(n)
