255e89af06d9fc59452dda9acdeeaa31f7c92804
FabPaul <paulfopa0@gmail.com>
2024-01-11 07:44:23 +0100

    concat anotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" A type-notation that concats 2 string args """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns the concatatenated string"""
+
+    return str1 + str2
