7ee264be8e6d7b76549c30bae6374aa4ec50ec15
FabPaul <paulfopa0@gmail.com>
2024-01-15 10:33:30 +0100

    random.random() takes no arg. since I want values between 0 - max_delay, i should instead use random.uniform

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """ waits for a random delay between 0 and max_delay and returns  it"""
-    random_delay = random.random(0, max_delay)
+    random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
