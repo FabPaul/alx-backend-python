a13b42e4507a5e2852a3e3142a8932e1e00cfd77
FabPaul <paulfopa0@gmail.com>
2024-02-04 17:59:42 +0100

    Memoization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,6 +16,7 @@ from parameterized import parameterized
 from utils import access_nested_map
 from unittest.mock import Mock, patch
 from utils import get_json
+from utils import memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -54,3 +55,37 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get', return_value=mock):
             response = get_json(url)
             self.assertEqual(response, expected_result)
+
+
+class TestMemoize(unittest.TestCase):
+    """Memoize class"""
+
+    def test_memoize(self):
+
+        class TestClass:
+            
+            def a_method(self):
+                """Memoize a_method"""
+                return 42
+    
+            @memoize
+            def a_property(self):
+                """Memoize a_property"""
+                return self.a_method()
+            
+        # Instance of testClass
+        test_instance = TestClass()
+
+        with patch.object(test_instance, 'a_method') as mock_a_method:
+            mock_a_method.return_value = 42
+
+            # assert that a_property is called 2wice
+            first_call = test_instance.a_property
+            second_call = test_instance.a_property
+
+            # assert that a_method is called only once
+            mock_a_method.assert_called_once()
+
+            # Be sure the results are cprrect
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
